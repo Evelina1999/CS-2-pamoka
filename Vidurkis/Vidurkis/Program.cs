e3d68e5f1d95f2314ade8ab93bb3f44451918901
Evelina1999 <evelincik0327@gmail.com>
2022-01-31 02:45:26 +0200

    Vidurkis

---
+++ Vidurkis/Vidurkis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidurkis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            a = 4;
+            b = 6;
+            c = 10;
+            Console.WriteLine("Vidurkis");
+            Console.WriteLine( (a + b + c) / 3 );
+            Console.ReadLine();
+
+  
+        
+
+        }
+    }
+}
