617299a4eee5660f435583ce55b3b41d9d793e6e
Evelina1999 <evelincik0327@gmail.com>
2022-01-31 02:27:06 +0200

    Suma, skirtumas, sandauga, dalyba

--- 2 pamoka/2 pamoka/Program.cs
+++ 2 pamoka/2 pamoka/Program.cs
@@ -10,6 +10,22 @@ namespace _2_pamoka
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ivesk skaiciu 1");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ivesk skaiciu 2");
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("skaiciu suma:");
+            Console.WriteLine(skaicius1 + skaicius2);
+            Console.WriteLine("skaiciu skirtumas:");
+            Console.WriteLine(skaicius1 - skaicius2);
+            Console.WriteLine("skaiciu sandauga:");
+            Console.WriteLine(skaicius1 * skaicius2);
+            Console.WriteLine("skaiciu dalyba:");
+            Console.WriteLine(skaicius1 / skaicius2);
+
+            Console.ReadLine(); 
+
         }
     }
 }
