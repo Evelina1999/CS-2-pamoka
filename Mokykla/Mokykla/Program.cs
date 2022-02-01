63e55b084fd81de451a59ddc5ecb5245e41a4c20
Evelina1999 <evelincik0327@gmail.com>
2022-02-02 01:33:50 +0200

    Mokykla

--- Mokykla/Mokykla/Program.cs
+++ Mokykla/Mokykla/Program.cs
@@ -10,6 +10,31 @@ namespace Mokykla
     {
         static void Main(string[] args)
         {
+            string mokyklosPavadinimas;
+            string kursoPavadinimas;
+            int studentuSkaicius;
+            Console.WriteLine(DateTime.Now);
+
+
+            Console.WriteLine("Iveskite mokyklos pavadinima:");
+            mokyklosPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite kurso pavadinima:");
+            kursoPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskkite studentu skaiciu:");
+            studentuSkaicius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(
+                "Mokyklos pavadinimas: {0}\n" +
+                "Kurso pavadinimas: {1}\n" +
+                "Studentu skaicius: {2}",
+                mokyklosPavadinimas,
+                kursoPavadinimas,
+                studentuSkaicius);
+            
+
+            Console.WriteLine(DateTime.Now);
+
+            Console.ReadLine(); 
         }
     }
 }
