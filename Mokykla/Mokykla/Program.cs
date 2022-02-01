aaa9805989518dbdd0a7092231076b2419ec202f
Evelina1999 <evelincik0327@gmail.com>
2022-02-02 01:57:57 +0200

    kosoles isvedimo budai

--- Mokykla/Mokykla/Program.cs
+++ Mokykla/Mokykla/Program.cs
@@ -10,31 +10,62 @@ namespace Mokykla
     {
         static void Main(string[] args)
         {
-            string mokyklosPavadinimas;
-            string kursoPavadinimas;
-            int studentuSkaicius;
-            Console.WriteLine(DateTime.Now);
+            //string mokyklosPavadinimas;
+            //string kursoPavadinimas;
+            //int studentuSkaicius;
+            //Console.WriteLine(DateTime.Now);
 
 
-            Console.WriteLine("Iveskite mokyklos pavadinima:");
-            mokyklosPavadinimas = Console.ReadLine();
-            Console.WriteLine("Iveskite kurso pavadinima:");
-            kursoPavadinimas = Console.ReadLine();
-            Console.WriteLine("Iveskkite studentu skaiciu:");
-            studentuSkaicius = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Iveskite mokyklos pavadinima:");
+            //mokyklosPavadinimas = Console.ReadLine();
+            //Console.WriteLine("Iveskite kurso pavadinima:");
+            //kursoPavadinimas = Console.ReadLine();
+            //Console.WriteLine("Iveskkite studentu skaiciu:");
+            //studentuSkaicius = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(
-                "Mokyklos pavadinimas: {0}\n" +
-                "Kurso pavadinimas: {1}\n" +
-                "Studentu skaicius: {2}",
-                mokyklosPavadinimas,
-                kursoPavadinimas,
-                studentuSkaicius);
+            //Console.WriteLine(
+            //    "Mokyklos pavadinimas: {0}\n" +
+            //    "Kurso pavadinimas: {1}\n" +
+            //    "Studentu skaicius: {2}",
+            //    mokyklosPavadinimas,
+            //    kursoPavadinimas,
+            //    studentuSkaicius);
             
 
-            Console.WriteLine(DateTime.Now);
+            //Console.WriteLine(DateTime.Now);
 
-            Console.ReadLine(); 
+            //Console.ReadLine();
+
+            string mokyklosPavadinimas;
+            string kursoPabadinimas;
+            int studentuSkaicius;
+            DateTime kursoPradziosData = new DateTime(2019, 12, 10);
+            DateTime kursoPabaigosData = new DateTime(2020, 2, 25);
+
+
+            double dienuSkirtumas = (kursoPabaigosData - kursoPradziosData).TotalDays;
+            double savaiciuSkaicius = dienuSkirtumas / 7;
+            double dienuSkaiciusBeSavaitgaliu = savaiciuSkaicius * 4;
+
+            Console.WriteLine("Iveskite mokyklos pavadinima");
+            mokyklosPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite kursopavadinima");
+            kursoPabadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite studentu skaiciu");
+            studentuSkaicius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Mokyklos pavadinimas: " + mokyklosPavadinimas);
+            Console.WriteLine("Kurso pavadinimas: " + kursoPabadinimas);
+            Console.WriteLine("Stdentu skaicius: " + studentuSkaicius);
+            Console.WriteLine("Siandienos data: " + DateTime.Now.ToString());
+            Console.WriteLine("Kurso pradzios data: " + kursoPradziosData);
+            Console.WriteLine("Kurso pabaigos data: " + kursoPabaigosData);
+            Console.WriteLine("Dienu skirtumas tarp kuso pradzios ir pabaigos: " + dienuSkirtumas);
+            Console.WriteLine("Savaiciu skaicius: " + savaiciuSkaicius);
+            Console.WriteLine("Darbo dienu skaicius: " + dienuSkaiciusBeSavaitgaliu);
+            Console.ReadLine();
+
+
         }
     }
 }
