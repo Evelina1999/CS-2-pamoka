cd53f6e8229f84dcb8487a7d3f49735a681bbbbe
Evelina1999 <evelincik0327@gmail.com>
2022-02-02 16:13:42 +0200

    Kintamieji

---
+++ Kintamieji/Kintamieji/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 15;
+            int C;
+
+            Console.WriteLine($"A = {A} \nB = {B}");
+
+            C = A;
+            A = B;
+            B = C;
+
+            Console.WriteLine($"A = {A} \nB = {B}");
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
