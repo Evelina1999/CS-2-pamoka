6724a35ce7704b3446f784d3295da0363a64f4c9
Evelina1999 <evelincik0327@gmail.com>
2022-02-03 20:49:08 +0200

    init

---
+++ Sekundes/Sekundes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sekundes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ivesk skaiciu (sekundes)");
+            int sekundes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Valandu: {sekundes / 3600}");
+            Console.WriteLine($"Minuciu: {sekundes / 60}");
+            Console.WriteLine($"SEkundziu: {sekundes}");
+
+            Console.ReadLine(); 
+        }
+    }
+}
