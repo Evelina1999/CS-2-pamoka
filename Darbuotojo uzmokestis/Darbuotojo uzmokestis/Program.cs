c8c65e87abe744435157b61946d405a2040a17fd
Evelina1999 <evelincik0327@gmail.com>
2022-02-03 19:39:25 +0200

    valandinis

---
+++ Darbuotojo uzmokestis/Darbuotojo uzmokestis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darbuotojo_uzmokestis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {  
+            
+            
+            
+
+            Console.WriteLine("Ivesti darbuotojo ID:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ivesti isdirbtas valandas:");
+            decimal valandos = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Ivesti valandini:");
+            decimal valandinis = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Darbuotojo alga: {valandos * valandinis} ");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
