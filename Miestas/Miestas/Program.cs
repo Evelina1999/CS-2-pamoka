160ad6e66f4076b76179ace5a561087bb800009b
Evelina1999 <evelincik0327@gmail.com>
2022-02-03 17:10:22 +0200

    Miestas, gatve, pastatymo metai

--- Miestas/Miestas/Program.cs
+++ Miestas/Miestas/Program.cs
@@ -10,6 +10,25 @@ namespace Miestas
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Suvesk miesto pavadinima:");
+            string miestoPavadinimas = Console.ReadLine();
+            Console.WriteLine("Suvesk gatves pavadinima:");
+            string gatvesPavadinimas = Console.ReadLine();
+            Console.WriteLine("Suvesk namo nr:");
+            int namoNr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Suvesk pastatymo metus:");
+            DateTime pastatymoMetai = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine($"Miesto pavadinimas: {miestoPavadinimas}\n" +
+                $"Gatves pavadinimas: {gatvesPavadinimas}\n" +
+                $"Namo numeris: {namoNr}\n" +
+                $"Namo pastatymo metai (pvz. 2022/02/03): {pastatymoMetai.Year}");
+
+            Console.ReadLine(); 
+                
+
+
+
         }
     }
 }
