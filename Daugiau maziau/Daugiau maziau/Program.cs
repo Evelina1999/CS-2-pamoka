cc31b04eb15cd4982aa89c92502646a3b3943bfd
Evelina1999 <evelincik0327@gmail.com>
2022-02-03 16:02:53 +0200

    Daugiau maziau

---
+++ Daugiau maziau/Daugiau maziau/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daugiau_maziau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius;
+
+            Console.WriteLine("Ivesk skaiciu:");
+            skaicius = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine((skaicius > 0)); 
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
