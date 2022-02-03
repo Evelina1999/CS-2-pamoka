997d80ef1d535fbfcf4d4516d87598dda5ca034f
Evelina1999 <evelincik0327@gmail.com>
2022-02-03 18:29:30 +0200

    Didziosios raides

---
+++ Didziosios raides/Didziosios raides/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Didziosios_raides
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite varda");
+            string vardas = Console.ReadLine().ToUpper();
+            
+            Console.WriteLine("Iveskite pavarde");
+            string pavarde = Console.ReadLine().ToUpper();
+            
+
+            Console.WriteLine($"{vardas}_{pavarde}" );
+            Console.ReadLine();
+
+            
+
+            
+        }
+    }
+}
