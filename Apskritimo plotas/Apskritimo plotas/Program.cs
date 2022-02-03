9164e791cf5472cea2c3d5818e2fa4843e40d304
Evelina1999 <evelincik0327@gmail.com>
2022-02-03 18:44:28 +0200

    Formule C=d*pi

---
+++ Apskritimo plotas/Apskritimo plotas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apskritimo_plotas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ivesk apskritimo spinduli");
+            decimal apskritimoSpindulys = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"Apskritimo ilgis: {(apskritimoSpindulys * 2) * Convert.ToDecimal(Math.PI)}");
+
+            Console.ReadLine();
+        }
+    }
+}
