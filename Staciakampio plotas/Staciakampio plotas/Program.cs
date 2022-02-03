f782f9a831aa81d4c04d2587ed7bd65795216150
Evelina1999 <evelincik0327@gmail.com>
2022-02-03 03:32:32 +0200

    Plotas

--- Staciakampio plotas/Staciakampio plotas/Program.cs
+++ Staciakampio plotas/Staciakampio plotas/Program.cs
@@ -9,7 +9,19 @@ namespace Staciakampio_plotas
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {  
+            int ilgis;
+            int plotis;
+
+            Console.WriteLine("Iveskite staciakampio ilgi");
+            ilgis = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Iveskite staciakampio ploti");
+            plotis = Convert.ToInt32(Console.Read());
+            Console.WriteLine("cm");
+            Console.WriteLine($"Plotas: {ilgis * plotis}"); 
+
+            Console.ReadLine();
         }
     }
 }
